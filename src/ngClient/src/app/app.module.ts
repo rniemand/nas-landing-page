e1982fb7d2d9fdba61fd12b523bdf8df212d895d
Richard Niemand <niemand.richard@gmail.com>
2021-08-10 17:09:10 -0600

    Stubbing modules

--- src/ngClient/src/app/app.module.ts
+++ src/ngClient/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Custom modules
+import { MaterialModule } from './modules/material.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
