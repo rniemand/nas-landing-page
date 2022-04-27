e24cd249d910f322775cb0547d81645a4a897abf
Richard Niemand <niemand.richard@gmail.com>
2022-04-26 20:43:53 -0600

    dogs balls

--- src/NasLandingPage/wwwroot/libs/rn.enum.js
+++ src/NasLandingPage/wwwroot/libs/rn.enum.js
@@ -1,19 +1,24 @@
-var rn = rn || {};
-rn.enums = rn.enums || {};
+(function(global) {
+  global.rn = global.rn || {};
+  global.rn.enums = global.rn.enums || {};
 
-rn.enums.TableColumn = rn.enums.TableColumn || {
-  Name: 1,
-  Visibility: 2,
-  Code: 3,
-  Action: 4,
-  SonarQube: 5,
-  Badges: 6,
-  Readme: 7,
-  GitAttributes: 8,
-  EditorConfig: 9,
-  PrTemplate: 10,
-  License: 11,
-  HasBuildScripts: 12,
-  BuildScriptsVersion: 13,
-  ProjectType: 14
-};
+  var enums = global.rn.enums;
+  
+  enums.TableColumn = {
+    Name: 1,
+    Visibility: 2,
+    Code: 3,
+    Action: 4,
+    SonarQube: 5,
+    Badges: 6,
+    Readme: 7,
+    GitAttributes: 8,
+    EditorConfig: 9,
+    PrTemplate: 10,
+    License: 11,
+    HasBuildScripts: 12,
+    BuildScriptsVersion: 13,
+    ProjectType: 14
+  };
+  
+}(window));
