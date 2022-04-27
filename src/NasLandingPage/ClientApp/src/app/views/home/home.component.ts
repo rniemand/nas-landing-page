ff591cf1d2535bafd53f2dd0534cd16bcf1658b7
Richard Niemand <niemand.richard@gmail.com>
2022-04-27 16:58:26 -0600

    Before swagger

--- src/NasLandingPage/ClientApp/src/app/views/home/home.component.ts
+++ src/NasLandingPage/ClientApp/src/app/views/home/home.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    // http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+    //   this.forecasts = result;
+    // }, error => console.error(error));
+  }
 }
