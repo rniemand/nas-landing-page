faa2c8a5b2f9c469c6db3f85d3c7522854a2ac33
Richard Niemand <niemand.richard@gmail.com>
2022-04-27 19:44:48 -0600

    Semi working

--- src/NasLandingPage/ClientApp/src/app/components/array-count/array-count.component.ts
+++ src/NasLandingPage/ClientApp/src/app/components/array-count/array-count.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
-  selector: 'app-array-count',
+  selector: 'array-count',
   templateUrl: './array-count.component.html',
   styleUrls: ['./array-count.component.scss']
 })
-export class ArrayCountComponent implements OnInit {
+export class ArrayCountComponent implements OnInit, OnChanges {
+  @Input('value') value: any[] = [];
+  count: number = 0;
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+  ngOnChanges(_: SimpleChanges): void {
+    this.count = this.value.length;
   }
 
 }
