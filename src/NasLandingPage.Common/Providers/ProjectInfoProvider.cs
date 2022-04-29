b8a763846463fbb8210cad298653d8c66dda5a99
Richard Niemand <niemand.richard@gmail.com>
2022-04-29 13:34:39 -0600

    Extending FileSystemHelper

--- src/NasLandingPage.Common/Providers/ProjectInfoProvider.cs
+++ src/NasLandingPage.Common/Providers/ProjectInfoProvider.cs
@@ -3,8 +3,6 @@ using NasLandingPage.Common.Config;
 using NasLandingPage.Common.Helpers;
 using NasLandingPage.Common.Models.Responses;
 using NasLandingPage.Common.Models.Responses.Projects;
-using Rn.NetCore.Common.Abstractions;
-using Rn.NetCore.Common.Helpers;
 
 namespace NasLandingPage.Common.Providers;
 
@@ -19,11 +17,6 @@ public interface IProjectInfoProvider
 public class ProjectInfoProvider : IProjectInfoProvider
 {
   private readonly IFileSystemHelper _fsHelper;
-  private readonly IDirectoryAbstraction _directory;
-  private readonly IEnvironmentAbstraction _environment;
-  private readonly IFileAbstraction _file;
-  private readonly IJsonHelper _jsonHelper;
-  private readonly IPathAbstraction _path;
   private readonly NasLandingPageConfig _config;
   private readonly string _dataDir;
   private readonly string _backupDir;
@@ -32,11 +25,6 @@ public class ProjectInfoProvider : IProjectInfoProvider
   {
     // TODO: [ProjectInfoProvider.ProjectInfoProvider] (TESTS) Add tests
     _fsHelper = serviceProvider.GetRequiredService<IFileSystemHelper>();
-    _directory = serviceProvider.GetRequiredService<IDirectoryAbstraction>();
-    _environment = serviceProvider.GetRequiredService<IEnvironmentAbstraction>();
-    _file = serviceProvider.GetRequiredService<IFileAbstraction>();
-    _jsonHelper = serviceProvider.GetRequiredService<IJsonHelper>();
-    _path = serviceProvider.GetRequiredService<IPathAbstraction>();
     _config = serviceProvider.GetRequiredService<INasLandingPageConfigProvider>().Provide();
 
     _dataDir = GenerateDataDirPath();
@@ -47,28 +35,19 @@ public class ProjectInfoProvider : IProjectInfoProvider
   public List<ProjectInfo> GetAll()
   {
     // TODO: [ProjectInfoProvider.GetAll] (TESTS) Add tests
-    var projects = new List<ProjectInfo>();
+    var files = _fsHelper.DirectoryGetFiles(_dataDir, "*.json", SearchOption.TopDirectoryOnly);
 
-    var files = _directory.GetFiles(_dataDir, "*.json", SearchOption.TopDirectoryOnly);
-
-    foreach (var file in files)
-    {
-      if (string.IsNullOrWhiteSpace(file)) continue;
-      projects.Add(LoadProjectFile(file));
-    }
-
-    return projects;
+    return files
+      .Where(file => !string.IsNullOrWhiteSpace(file))
+      .Select(LoadProjectFile)
+      .ToList();
   }
 
   public ProjectInfo? GetByName(string name)
   {
     // TODO: [ProjectInfoProvider.GetByName] (TESTS) Add tests
     var filePath = GenerateProjectFilePath(name);
-
-    if (!_file.Exists(filePath))
-      return null;
-
-    return LoadProjectFile(filePath);
+    return !_fsHelper.FileExists(filePath) ? null : LoadProjectFile(filePath);
   }
 
   public void UpdateProjectInfo(ProjectInfo projectInfo)
@@ -77,66 +56,36 @@ public class ProjectInfoProvider : IProjectInfoProvider
     var sourceFilePath = GenerateProjectFilePath(projectInfo.Metadata.FileNameWithoutExtension);
     var backupFilePath = sourceFilePath.Replace(_dataDir, _backupDir);
 
-    if (!BackupFile(sourceFilePath, backupFilePath))
+    if (!_fsHelper.BackupFileToFolder(sourceFilePath, backupFilePath))
     {
       // TODO: [ProjectInfoProvider.UpdateProjectInfo] (EX) Use better exception here
       throw new Exception("Unable to backup file");
     }
 
     projectInfo.Metadata = new ProjectInfoMetadata();
-    var projectJson = _jsonHelper.SerializeObject(projectInfo, true);
-    _file.WriteAllText(sourceFilePath, projectJson);
+    _fsHelper.SaveJsonFile(sourceFilePath, projectInfo, true);
   }
 
-  public List<string> ListProjectFiles()
-  {
-    // TODO: [ProjectInfoProvider.ListProjectFiles] (TESTS) Add tests
-    var projectFiles = new List<string>();
-    var files = _directory.GetFiles(_dataDir, "*.json", SearchOption.TopDirectoryOnly);
-
-    foreach (var file in files)
-    {
-      var fileName = _path.GetFileNameWithoutExtension(file);
-      projectFiles.Add(fileName);
-    }
-
-    return projectFiles;
-  }
+  public List<string> ListProjectFiles() =>
+    // TODO: [ProjectInfoProvider.ListDirectoryFiles] (TESTS) Add tests
+    _fsHelper.ListDirectoryFiles(_dataDir, true);
 
 
-  private bool BackupFile(string source, string destination)
-  {
-    // TODO: [ProjectInfoProvider.BackupFile] (TESTS) Add tests
-    if (!_file.Exists(source))
-    {
-      return false;
-    }
-
-    if (_file.Exists(destination))
-    {
-      _file.Delete(destination);
-    }
-
-    _file.Move(source, destination);
-    return true;
-  }
-
-  private string GenerateProjectFilePath(string name)
-  {
+  private string GenerateProjectFilePath(string name) =>
     // TODO: [ProjectInfoProvider.GenerateProjectFilePath] (TESTS) Add tests
-    return $"{_dataDir}{name}.json";
-  }
+    $"{_dataDir}{name}.json";
 
   private ProjectInfo LoadProjectFile(string path)
   {
     // TODO: [ProjectInfoProvider.LoadProjectFile] (TESTS) Add tests
-    var fileJson = _file.ReadAllText(path);
-    var projectInfo = _jsonHelper.DeserializeObject<ProjectInfo>(fileJson);
+    var projectInfo = _fsHelper.LoadJsonFile<ProjectInfo>(path);
+    if (projectInfo is null)
+      throw new Exception("Unable to load project file");
 
     projectInfo.Metadata = new ProjectInfoMetadata
     {
-      FileName = _path.GetFileName(path),
-      FileNameWithoutExtension = _path.GetFileNameWithoutExtension(path)
+      FileName = _fsHelper.GetFileName(path),
+      FileNameWithoutExtension = _fsHelper.GetFileNameWithoutExtension(path)
     };
 
     return projectInfo;
@@ -146,7 +95,7 @@ public class ProjectInfoProvider : IProjectInfoProvider
   {
     // TODO: [ProjectInfoProvider.GenerateDataDirPath] (TESTS) Add tests
     var sep = _config.IsLinux ? "/" : "\\";
-    var rootDir = _environment.CurrentDirectory;
+    var rootDir = _fsHelper.CurrentDirectory;
 
     if (!rootDir.EndsWith(sep))
       rootDir += sep;
@@ -167,7 +116,7 @@ public class ProjectInfoProvider : IProjectInfoProvider
   {
     // TODO: [ProjectInfoProvider.GenerateBackupDirPath] (TESTS) Add tests
     var sep = _config.IsLinux ? "/" : "\\";
-    var rootDir = _environment.CurrentDirectory;
+    var rootDir = _fsHelper.CurrentDirectory;
 
     if (!rootDir.EndsWith(sep))
       rootDir += sep;
