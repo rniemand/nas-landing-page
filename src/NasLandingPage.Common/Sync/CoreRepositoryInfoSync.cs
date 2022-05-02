9ca6bba33732df138991585464bc7147f0a4aa8d
Richard Niemand <niemand.richard@gmail.com>
2022-05-02 10:48:42 -0600

    Breaking up CoreRepositoryInfoSync

--- src/NasLandingPage.Common/Sync/CoreRepositoryInfoSync.cs
+++ src/NasLandingPage.Common/Sync/CoreRepositoryInfoSync.cs
@@ -13,73 +13,129 @@ public static class CoreRepositoryInfoSync
     projectInfo.Repo.LastUpdated = repository.UpdatedAt;
     projectInfo.Languages = new[] { repository.Language };
 
-    if (projectInfo.Repo.DefaultBranch != repository.DefaultBranch)
-    {
-      messages.Add($"Set default branch to: {repository.DefaultBranch}");
-      projectInfo.Repo.DefaultBranch = repository.DefaultBranch;
-    }
-
-    var repoIsPublic = repository.Visibility == RepositoryVisibility.Public;
-    if (projectInfo.Repo.IsPublic != repoIsPublic)
-    {
-      messages.Add("Set visibility to: " + (repoIsPublic ? "public" : "private"));
-      projectInfo.Repo.IsPublic = repoIsPublic;
-    }
-
-    if (projectInfo.Repo.ForksCount != repository.ForksCount)
-    {
-      messages.Add($"Updated forked count to: {repository.ForksCount}");
-      projectInfo.Repo.ForksCount = repository.ForksCount;
-    }
-
-    if (projectInfo.Repo.HtmlUrl != repository.HtmlUrl)
-    {
-      messages.Add($"Set Html URL to: {repository.HtmlUrl}");
-      projectInfo.Repo.HtmlUrl = repository.HtmlUrl;
-    }
-
-    if (projectInfo.Repo.FullName != repository.FullName)
-    {
-      messages.Add($"Set full name to: {repository.FullName}");
-      projectInfo.Repo.FullName = repository.FullName;
-    }
-
-    if (projectInfo.Repo.GitUrl != repository.GitUrl)
-    {
-      messages.Add($"Updated git URL to: {repository.GitUrl}");
-      projectInfo.Repo.GitUrl = repository.GitUrl;
-    }
-
-    if (projectInfo.Repo.OpenIssuesCount != repository.OpenIssuesCount)
-    {
-      messages.Add($"Updated open issue count to: {repository.OpenIssuesCount}");
-      projectInfo.Repo.OpenIssuesCount = repository.OpenIssuesCount;
-    }
-
-    if (projectInfo.Repo.SshUrl != repository.SshUrl)
-    {
-      messages.Add($"Updated SSH URL to: {repository.SshUrl}");
-      projectInfo.Repo.SshUrl = repository.SshUrl;
-    }
-
-    if (projectInfo.Repo.ApiUrl != repository.Url)
-    {
-      messages.Add($"Updated API url to: {repository.Url}");
-      projectInfo.Repo.ApiUrl = repository.Url;
-    }
-
-    if (projectInfo.Repo.Size != repository.Size)
-    {
-      messages.Add($"Updated repo size to: {repository.Size}");
-      projectInfo.Repo.Size = repository.Size;
-    }
-
-    if (projectInfo.Description != repository.Description)
-    {
-      messages.Add($"Updated description to: {repository.Description}");
-      projectInfo.Description = repository.Description;
-    }
+    SyncDefaultBranch(messages, projectInfo, repository);
+    SyncIsPublic(messages, projectInfo, repository);
+    SyncForkCount(messages, projectInfo, repository);
+    SyncHtmlUrl(messages, projectInfo, repository);
+    SyncFullName(messages, projectInfo, repository);
+    SyncGitUrl(messages, projectInfo, repository);
+    SyncOpenIssueCount(messages, projectInfo, repository);
+    SyncSshUrl(messages, projectInfo, repository);
+    SyncApiUrl(messages, projectInfo, repository);
+    SyncRepoSize(messages, projectInfo, repository);
+    SyncDescription(messages, projectInfo, repository);
 
     return messages;
+  }
+
+  private static void SyncDefaultBranch(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncDefaultBranch] (TESTS) Add tests
+    if (projectInfo.Repo.DefaultBranch == repository.DefaultBranch)
+      return;
+
+    messages.Add($"Set default branch to: {repository.DefaultBranch}");
+    projectInfo.Repo.DefaultBranch = repository.DefaultBranch;
+  }
+
+  private static void SyncIsPublic(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncIsPublic] (TESTS) Add tests
+    var repoIsPublic = repository.Visibility == RepositoryVisibility.Public;
+    if (projectInfo.Repo.IsPublic == repoIsPublic)
+      return;
+
+    messages.Add("Set visibility to: " + (repoIsPublic ? "public" : "private"));
+    projectInfo.Repo.IsPublic = repoIsPublic;
+  }
+
+  private static void SyncForkCount(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncForkCount] (TESTS) Add tests
+    if (projectInfo.Repo.ForksCount == repository.ForksCount)
+      return;
+
+    messages.Add($"Updated forked count to: {repository.ForksCount}");
+    projectInfo.Repo.ForksCount = repository.ForksCount;
+  }
+
+  private static void SyncHtmlUrl(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncHtmlUrl] (TESTS) Add tests
+    if (projectInfo.Repo.HtmlUrl == repository.HtmlUrl)
+      return;
+
+    messages.Add($"Set Html URL to: {repository.HtmlUrl}");
+    projectInfo.Repo.HtmlUrl = repository.HtmlUrl;
+  }
+
+  private static void SyncFullName(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncFullName] (TESTS) Add tests
+    if (projectInfo.Repo.FullName == repository.FullName)
+      return;
+
+    messages.Add($"Set full name to: {repository.FullName}");
+    projectInfo.Repo.FullName = repository.FullName;
+  }
+
+  private static void SyncGitUrl(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncGitUrl] (TESTS) Add tests
+    if (projectInfo.Repo.GitUrl == repository.GitUrl)
+      return;
+
+    messages.Add($"Updated git URL to: {repository.GitUrl}");
+    projectInfo.Repo.GitUrl = repository.GitUrl;
+  }
+
+  private static void SyncOpenIssueCount(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncOpenIssueCount] (TESTS) Add tests
+    if (projectInfo.Repo.OpenIssuesCount == repository.OpenIssuesCount)
+      return;
+
+    messages.Add($"Updated open issue count to: {repository.OpenIssuesCount}");
+    projectInfo.Repo.OpenIssuesCount = repository.OpenIssuesCount;
+  }
+
+  private static void SyncSshUrl(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncSshUrl] (TESTS) Add tests
+    if (projectInfo.Repo.SshUrl == repository.SshUrl)
+      return;
+
+    messages.Add($"Updated SSH URL to: {repository.SshUrl}");
+    projectInfo.Repo.SshUrl = repository.SshUrl;
+  }
+
+  private static void SyncApiUrl(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncApiUrl] (TESTS) Add tests
+    if (projectInfo.Repo.ApiUrl == repository.Url)
+      return;
+
+    messages.Add($"Updated API url to: {repository.Url}");
+    projectInfo.Repo.ApiUrl = repository.Url;
+  }
+
+  private static void SyncRepoSize(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncRepoSize] (TESTS) Add tests
+    if (projectInfo.Repo.Size == repository.Size)
+      return;
+
+    messages.Add($"Updated repo size to: {repository.Size}");
+    projectInfo.Repo.Size = repository.Size;
+  }
+
+  private static void SyncDescription(ICollection<string> messages, ProjectInfo projectInfo, Repository repository)
+  {
+    // TODO: [CoreRepositoryInfoSync.SyncDescription] (TESTS) Add tests
+    if (projectInfo.Description == repository.Description)
+      return;
+
+    messages.Add($"Updated description to: {repository.Description}");
+    projectInfo.Description = repository.Description;
   }
 }
