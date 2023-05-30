b1ed426aed6feec940b695ce13f53fd4c3ca0a2d
Richard Niemand <niemand.richard@gmail.com>
2023-05-30 17:07:46 -0600

    Sveltkit working

--- src/NasLandingPage/Program.cs
+++ src/NasLandingPage/Program.cs
@@ -12,19 +12,23 @@ public class Program
     builder.Services.AddSwaggerDocument();
 
     var app = builder.Build();
+
     app.UseOpenApi();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-      app.UseSwaggerUi3();
-      app.UseSwaggerUI();
-    }
-
-    app.UseAuthorization();
-
-
+    app.UseSwaggerUi3();
     app.MapControllers();
+    app.UseRouting();
+    app.UseAuthorization();
+    app.UseEndpoints(_ => { });
+
+#if DEBUG
+    app.UseSpa(spa =>
+    {
+      spa.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+    });
+#else
+    app.UseStaticFiles();
+    app.MapFallbackToFile("index.html");
+#endif
 
     app.Run();
   }
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+  import { WeatherForecast, WeatherForecastClient } from "../nlp-api";
+
+  new WeatherForecastClient().get().then((forecast: WeatherForecast[]) => {
+    console.log('x', forecast);
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
