e93df3128f4e0ed9211cb644ed4744cd59cf4db9
Richard Niemand <niemand.richard@gmail.com>
2023-06-09 18:42:00 -0600

    Prepping for merge

--- docker/init/01-DevSeed.sql
+++ docker/init/01-DevSeed.sql
@@ -102,4 +102,4 @@ ENGINE=InnoDB
 INSERT INTO `Users`
 	(`Email`, `PasswordHash`)
 VALUES
-	('niemand.richard@gmail.com', '');
+	('niemand.richard@gmail.com', 'AQAAAAIAAYagAAAAEKGjbkZvWeUEDyWmUKEw7NFmLPKbDXSpIrquaQI6XcJCX4DAkA41pOqCmxaCPs7xbQ==');
