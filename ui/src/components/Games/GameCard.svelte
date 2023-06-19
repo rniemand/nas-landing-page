5501efd6a86b1ec025b26a6bf1cdc77054279982
Richard Niemand <niemand.richard@gmail.com>
2023-06-19 16:38:22 -0600

    Adds price and sold indicator

--- ui/src/components/Games/GameCard.svelte
+++ ui/src/components/Games/GameCard.svelte
@@ -1,17 +1,17 @@
 <style>
   .card {
     width: 200px;
-    border: 1px solid #8b8b8b;
+    border: 1px solid #3f3f3f;
     margin: 6px;
     border-radius: 6px;
-    background-color: #252525;
+    background-color: #161616;
+    box-shadow: 3px 3px 3px 0px rgb(18 18 18);
   }
   h2 { text-align: center; font-size: 1.2em; padding: 3px 2px; }
-  .image { margin: 3px; }
-  img.cover {
-    width: 100%;
-    border-radius: 5px;
-  }
+  .image { margin: 3px; position: relative; text-align: center; }
+  img.cover { width: auto; border-radius: 5px; max-height: 200px; }
+  .sold { opacity: 0.5; background-color: #37010e; }
+  .price { background-color: rgba(0,0,0,.61); border-radius: 3px; color: #00ff1f; padding: 3px; position: absolute; right: 5px; top: 5px; }
 </style>
 
 <script lang="ts">
@@ -20,9 +20,10 @@
   export let game: BasicGameInfoDto;
 </script>
 
-<div class="card">
+<div class="card" class:sold={game.gameSold}>
   <h2 class="ellipsis">{game.gameName}</h2>
   <div class="image">
+    {#if game.gamePrice > 0}<div class="price">{game.gamePrice}</div>{/if}
     <!-- svelte-ignore a11y-missing-attribute -->
     <img class="cover" src={`api/images/game/cover/${game.platformName}/${game.gameID}`} />
   </div>
