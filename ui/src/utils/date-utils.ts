6fbe53cc8beb8c53dadc2cab1c1040c31c964ec1
Richard Niemand <niemand.richard@gmail.com>
2023-07-24 23:45:25 +0200

    Stubbing new view

--- ui/src/utils/date-utils.ts
+++ ui/src/utils/date-utils.ts
@@ -4,10 +4,9 @@ export const humanTimeDiff = (date: Date) => {
   const now = new Date().getTime();
   const timeDiff = Math.floor((now - date.getTime()) / 1000); // seconds
   if(timeDiff < 60) return '< 1 min';
-  if(timeDiff < 3600) return `${Math.floor(timeDiff / 60)} min ago`;
+  if(timeDiff < 3600) return `${Math.floor(timeDiff / 60)} min(s) ago`;
   if(timeDiff < 86400) return `${Math.floor(timeDiff / 3600)} hours ago`;
   if(timeDiff < 604800) return `${Math.floor(timeDiff / 86400)} days ago`;
   if(timeDiff < 2419200) return `${Math.floor(timeDiff / 604800)} weeks ago`;
-
-  return `>> FIX <<`;
+  return `${date.getFullYear()}-${(date.getMonth()+'').padStart(2,'0')}-${(date.getDate()+'').padStart(2,'0')}`;
 }
