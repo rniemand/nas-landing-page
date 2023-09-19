e150511782fc1fdf2b61d335ac4fcfe2b9ab9ca4
Richard Niemand <niemand.richard@gmail.com>
2023-09-18 21:26:17 -0600

    Daily

--- ui/src/components/UserLinks/LinkCategories.svelte
+++ ui/src/components/UserLinks/LinkCategories.svelte
@@ -1,3 +1,31 @@
+<style>
+	.links-container {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+	}
+	.links-container .link {
+		flex: auto;
+		background-color: #2f2f2f;
+		border: 1px solid #979797;
+		border-radius: 8px;
+		padding: 3px;
+		color: white;
+		margin-right: 2px;
+		cursor: pointer;
+		min-width: 100px;
+	}
+	.links-container .link:last-child {
+		margin-right: 0;
+	}
+	.links-container .link:hover {
+		background-color: #222222;
+	}
+	.links-container .link.active {
+		background-color: rgb(38, 80, 25);
+	}
+</style>
+
 <script lang="ts">
 	export let categories: string[];
 	export let onCategorySelected: (category: string) => void;
@@ -5,14 +33,15 @@
 </script>
 
 <div class="mt-2">
-	<ul class="menu w-full menu-vertical lg:menu-horizontal bg-base-200 rounded-box justify-center">
+	<div class="links-container">
 		{#each categories as category}
-			<li class="nav-item">
-				<a
-					href="#!"
-					class:active={selectedCategory === category}
-					on:click={() => onCategorySelected(category)}>{category}</a>
-			</li>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div
+				class="link"
+				class:active={selectedCategory === category}
+				on:click={() => onCategorySelected(category)}>
+				{category}
+			</div>
 		{/each}
-	</ul>
+	</div>
 </div>
