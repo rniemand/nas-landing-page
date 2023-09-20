41949d8ddabdb15b9cb70605f278c6703b8a56fd
Richard Niemand <niemand.richard@gmail.com>
2023-09-20 17:20:43 -0600

    Reworking login page

--- ui/src/routes/login/+page.svelte
+++ ui/src/routes/login/+page.svelte
@@ -1,67 +1,30 @@
 <style>
 	.form-signin {
-		max-width: 330px;
+		max-width: 450px;
 		padding: 15px;
 		position: absolute;
 		top: 50%;
 		left: 50%;
 		-ms-transform: translate(-50%, -50%);
 		transform: translate(-50%, -50%);
-		font-family: Roboto, 'open sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-		font-size: 14px;
-	}
-
-	.container {
-		height: 100vh;
-		text-align: center;
-	}
-
-	.password {
-		width: 160px;
-		border: 1px solid #464646;
-		padding: 6px;
-		border-radius: 6px;
-		margin-bottom: 10px;
-	}
-
-	.error {
-		color: #f00;
-	}
-
-	.btn-login {
-		border: 1px solid #b7b7b7;
-		background-color: white;
-		padding: 4px 8px;
-		border-radius: 5px;
-		font-size: 1em;
-		cursor: pointer;
-	}
-
-	.btn-login:hover {
-		background-color: #f7f7f7;
+		border: 1px solid #858484;
+		border-radius: 10px;
 	}
 	a {
+		display: block;
+		text-align: center;
 		text-decoration: none;
-		color: #1453a7;
-		font-weight: bold;
-	}
-	a:hover {
-		text-decoration: underline;
-		color: red;
-	}
-	.pad-bottom {
-		margin-bottom: 12px;
+		margin-bottom: 0.4em;
 	}
 </style>
 
 <script lang="ts">
-	import { AuthClient } from '../../nlp-api';
+	import { Input, Alert, Button, Form } from 'sveltestrap';
+	import { AuthClient, WhoAmIResponse } from '../../nlp-api';
 
-	const authClient = new AuthClient();
-	let username = '';
-	let email = '';
-	let password = '';
 	let passInput: HTMLInputElement | null = null;
+	let whoAmI: WhoAmIResponse | undefined = undefined;
+	let password = '';
 	let wrongCredentials = false;
 	let status = '';
 
@@ -75,77 +38,61 @@
 
 	triggerWrongCredentialsMessage._handle = <number | null>null;
 
-	function clearWrongCredentialsMessage() {
+	const clearWrongCredentialsMessage = () => {
 		wrongCredentials = false;
 		triggerWrongCredentialsMessage._handle = null;
-	}
+	};
 
 	(async () => {
-		const resp = await authClient.challenge(false);
-		username = resp.name!;
-		email = resp.email!;
-		if (resp.signedIn) location.href = '/';
+		whoAmI = await new AuthClient().challenge(false);
+		if (whoAmI.signedIn) location.href = '/';
 	})();
 
-	const triggerLogin = () => {
+	const triggerLogin = async () => {
 		if (status) return false;
 		clearWrongCredentialsMessage();
 		status = 'Validating credentials...';
-		(async () => {
-			try {
-				status = 'Logging in...';
-				await authClient.login(password);
-				setTimeout(() => {
-					location.reload();
-				}, 100);
-			} catch (ex) {
-				triggerWrongCredentialsMessage();
-				status = '';
-			}
-		})();
+		try {
+			status = 'Logging in...';
+			await new AuthClient().login(password);
+			setTimeout(() => location.reload(), 100);
+		} catch (ex) {
+			triggerWrongCredentialsMessage();
+			status = '';
+		}
 		return false;
 	};
 
-	function triggerGoogleSelectAccount() {
-		authClient.challenge(true);
-	}
+	const triggerGoogleSelectAccount = () => new AuthClient().challenge(true);
 
 	$: passInput && passInput.focus();
 </script>
 
-{#if email}
+{#if whoAmI && whoAmI.email}
 	<div class="container">
-		<main class="form-signin w-100 m-auto">
-			<form on:submit={triggerLogin}>
-				<h2 class="h3 mb-3 fw-normal">Welcome <strong>{username}</strong>!</h2>
+		<main class="form-signin w-100 m-auto shadow">
+			<Form on:submit={triggerLogin}>
+				<h3 class="text-center mb-3">Welcome <strong>{whoAmI.name}</strong>!</h3>
 				{#if status}
-					<div>{status}</div>
+					<p class="text-center">{status}</p>
 				{:else}
 					{#if wrongCredentials}
-						<div class="error">
-							Wrong password or no account with email {email}!
-						</div>
+						<Alert color="warning">
+							Wrong password or no account with email {whoAmI.email}!
+						</Alert>
 					{/if}
-					<div>
-						<input
-							type="password"
-							class="form-control password"
-							placeholder="Password"
-							bind:value={password}
-							bind:this={passInput} />
+					<Input type="password" class="mb-3" placeholder="Password" bind:value={password} />
+					<a href="#!" on:click={triggerGoogleSelectAccount}
+						>Not <strong>{whoAmI.email}</strong>?</a>
+					<a href="/reset-pass">First time visitor? Forgot your password?</a>
+					<div class="text-end">
+						<Button type="submit" class="w-full" color="primary" disabled={password.length == 0}
+							>Sign In</Button>
 					</div>
-
-					<div class="pad-bottom">
-						<a href="#!" on:click={triggerGoogleSelectAccount}>Not <strong>{email}</strong>?</a>
-					</div>
-					<div class="pad-bottom">
-						<a href="/reset-pass">First time visitor? Forgot your password?</a>
-					</div>
-					<button type="submit" class="btn-login">Sign in</button>
 				{/if}
-			</form>
+			</Form>
 		</main>
 	</div>
 {:else}
-	<h1>Please wait...</h1>
+	<h3 class="text-center mb-3">Please wait...</h3>
 {/if}
