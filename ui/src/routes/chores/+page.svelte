92dcdfb2739fe9961f25032fb8a7671c7eec6c7c
Richard Niemand <niemand.richard@gmail.com>
2023-10-13 14:37:55 -0600

    Stubbing AddChore modal

---
+++ ui/src/routes/chores/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Col, Row } from 'sveltestrap';
+	import AddChoreModal from './modals/AddChoreModal.svelte';
+</script>
+
+<Row>
+	<Col class="text-end">
+		<AddChoreModal />
+	</Col>
+</Row>
---
+++ ui/src/routes/chores/modals/AddChoreModal.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Button, Modal, ModalBody, ModalHeader } from 'sveltestrap';
+
+	let open = false;
+
+	const toggle = () => {
+		open = !open;
+	};
+</script>
+
+<Button color="success" on:click={toggle}>Add Chore</Button>
+<Modal isOpen={open} {toggle}>
+	<ModalHeader>Add Chore</ModalHeader>
+	<ModalBody>Here</ModalBody>
+</Modal>
