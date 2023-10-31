b0211ddec781322150d043dc7354347b3ff6ae2f
Richard Niemand <niemand.richard@gmail.com>
2023-10-31 16:26:56 -0600

    Updates seeding script - ensures that a correct default room is always selected

--- docker/init/20-seed-core.sql
+++ docker/init/20-seed-core.sql
@@ -73,5 +73,13 @@ INSERT INTO `HomeChores`
   (`RoomId`, `ChorePoints`, `Priority`, `IntervalModifier`, `Interval`, `ChoreName`, `ChoreDescription`)
 VALUES
   -- Main Home | Basement | Richard Office
-  (1, 1, 'low', 'Weeks', '1', 'Weekly Chore 1', 'Description')
+  (1, 1, 'low',   'Weeks',        '1',        'Weekly Chore 1',       'Occurs every week'),
+  -- Main Home | Basement | Server Room
+  (2, 1, 'low',   'DaysOfWeek',   'Mon,Fri',  'Server Room Chore 1',  'Occurs every Monday and Friday'),
+  -- Main Home | Basement | Store Room
+  (3, 1, 'med',   'DaysOfMonth',  '1,15',     'Store Room Chore 1',   'Occurs on the 1st and 15th of every month'),
+  -- Main Home | Main Floor | Dining Room
+  (4, 1, 'high',  'Days',         '2',        'Clean Dining Room',    'Occurs every second day'),
+  -- Main Home | Main Floor | Entryway
+  (7, 1, 'med',   'Weeks',        '2',        'Clean Door Glass',     'Occurs every 2 weeks')
 ;
--- ui/src/components/core/HomeRoomSelector.svelte
+++ ui/src/components/core/HomeRoomSelector.svelte
@@ -8,10 +8,17 @@
 	let loading: boolean = true;
 	let rooms: HomeRoomDto[] = [];
 
+	const isExistingRoom = (value: number) => {
+		if (value <= 0) return false;
+		const roomById = rooms.filter((x) => x.roomId === value);
+		return roomById.length === 1;
+	};
+
 	const refreshRooms = async (_floorId: number) => {
 		loading = true;
 		rooms = (await new RoomClient().getFloorRooms(_floorId)) || [];
-		if (rooms.length > 0 && value <= 0) value = rooms[0].roomId;
+		// Ensure that there is always a vaild default room selected
+		if (rooms.length > 0 && !isExistingRoom(value)) value = rooms[0].roomId;
 		loading = false;
 	};
 
