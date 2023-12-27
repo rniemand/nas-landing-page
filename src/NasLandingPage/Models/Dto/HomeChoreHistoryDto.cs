aa4c15a58c4f223ed3c6e4142c932f4a1b4e1492
Richard Niemand <niemand.richard@gmail.com>
2023-12-27 11:56:32 -0700

    Removes points claimed column

--- src/NasLandingPage/Models/Dto/HomeChoreHistoryDto.cs
+++ src/NasLandingPage/Models/Dto/HomeChoreHistoryDto.cs
@@ -5,7 +5,6 @@ public class HomeChoreHistoryDto
   public long ChoreHistoryId { get; set; }
   public int ChoreId { get; set; }
   public int UserId { get; set; }
-  public bool PointsClaimed { get; set; }
   public int Points { get; set; }
   public DateOnly DateAdded { get; set; } = DateOnly.MinValue;
   public DateOnly? DateClaimed { get; set; }
